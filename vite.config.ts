023236e786f785dba0f641e5e7bbb7cbaf10ab05
conterGui <conterguilherme22@gmail.com>
2026-01-15 20:22:00 +0000

    ready deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/\
 export default defineConfig(({ mode }) => ({
-  server: {
-    port: 8080,            // a porta que você já está usando
-    strictPort: true,
-    host: '0.0.0.0',             // permite conexões externas
-    allowedHosts: [
-      'stereotomical-octavia-propositionally.ngrok-free.dev'
-    ]
-  },
-  assetsInclude: ["**/*.JPG"],
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: mode === 'production' ? '/imovel-nacoes/' : '/',
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: 'docs',     
+    emptyOutDir: true,  
+    assetsDir: 'assets',
   },
 }));
