6f9c1658801e3c576399b241043bff3599a29f6b
conterGui <conterguilherme22@gmail.com>
2026-01-15 20:20:10 +0000

    ready deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/\
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    port: 8080,            // a porta que você já está usando
+    strictPort: true,
+    host: '0.0.0.0',             // permite conexões externas
+    allowedHosts: [
+      'stereotomical-octavia-propositionally.ngrok-free.dev'
+    ]
   },
   assetsInclude: ["**/*.JPG"],
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
